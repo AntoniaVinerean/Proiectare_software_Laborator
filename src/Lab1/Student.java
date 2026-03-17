76e74b94049c50799afe85d691b362924d0ec9db
AntoniaVinerean <antoniateodora.vinerean@ulbsibiu.ro>
2026-03-17 16:04:13 +0200

    Organizare proiect, Laborator 1 si Laborator 2.

---
+++ src/Lab1/Student.java
@@ -0,0 +1,22 @@
+package Lab1;
+public class Student {
+    private
+    String numarMatricol;
+    String nume;
+    String prenume;
+    String formatieDeStudiu;
+
+    public Student(String numarMatricol, String nume, String prenume, String formatieDeStudiu) {
+        this.numarMatricol = numarMatricol;
+        this.nume = nume;
+        this.prenume = prenume;
+        this.formatieDeStudiu = formatieDeStudiu;
+    }
+    @Override
+    public String toString() {
+        return "Student:" +" "+ "numarMatricol=" +" "+ numarMatricol + ", " +
+                "nume=" +" "+ nume + ", " +
+                "prenume=" +" "+ prenume + ", " +
+                "formatie de studiu=" +" "+ formatieDeStudiu;
+    }
+}
