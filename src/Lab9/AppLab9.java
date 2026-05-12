37cd9a2f2282af9755d9aea60f32f806a1e53ff9
AntoniaVinerean <antoniateodora.vinerean@ulbsibiu.ro>
2026-05-12 14:15:23 +0300

    Lab 9 problema 9.3.1

---
+++ src/Lab9/AppLab9.java
@@ -0,0 +1,42 @@
+package Lab9;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import static java.util.Locale.filter;
+
+public class AppLab9 {
+    public static void main(String[] args) {
+        List<Integer> numere = new Random()
+                .ints(10, 5, 26)
+                .boxed()
+                .collect(Collectors.toList());
+
+        System.out.println("Numerele sunt: " +numere);
+        System.out.println(" ");
+
+        int suma = numere.stream().mapToInt(Integer::intValue).sum();
+        System.out.println("Suma numerelor este: "+suma);
+
+        int min =  numere.stream().mapToInt(Integer::intValue).min().getAsInt();
+        int max =  numere.stream().mapToInt(Integer::intValue).max().getAsInt();
+        System.out.println("Numarul minim este: "+min);
+        System.out.println("Numarul maxim este: "+max);
+
+        List <Integer> intervalNou = numere.stream()
+            .filter(n -> n >= 10 && n <= 20)
+                    .collect(Collectors.toList());
+        System.out.println("Lista filtrata in intervalul [10,20] este: "+intervalNou);
+
+        List <Double> numereDouble = numere.stream()
+                .map (n-> n.doubleValue())
+                .collect (Collectors.toList());
+        System.out.println("Lista transformata in Double: " + numereDouble);
+
+        boolean gasit12 = numere.stream()
+                .anyMatch(n -> n == 12);
+        System.out.println("Se gaseste valoarea 12 in lista? " + (gasit12 ? "Da" : "Nu"));
+
+
+    }
+}
+
